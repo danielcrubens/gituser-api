54be838de9510d55e0e010e2f20aff76437aa5a1
danielcrubens <dcr.tecnologi@gmail.com>
2023-07-25 17:10:59 -0300

    consumindo API :memo:

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 
 const darkMode = document.querySelector('#darkMode');
+const searchUser = document.querySelector('#searchUser')
+const searchInput = document.querySelector('#Search');
+const apiGitHub = "https://api.github.com/users/"
 
 function setDarkModeState(isDarkMode) {
   localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
@@ -30,3 +33,100 @@ darkMode.addEventListener('click', () => {
   DarkModeToggle();
   setDarkModeState(document.documentElement.classList.contains('dark'));
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  gitUser('octocat');
+});
+
+searchUser.addEventListener('click', () => {
+  const userInput = searchInput.value.trim();
+if (userInput !== '') {
+  gitUser(userInput);
+  document.querySelector(".notice").classList.add('hidden');
+
+
+} else {
+  document.querySelector(".notice").classList.remove('hidden');
+
+}
+})
+
+const gitUser = async (username) => {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`, {
+      cache: 'no-cache'
+    });
+    if (!res.ok) {
+      throw new Error('User not found.');
+    }
+    const data = await res.json()
+    console.log(data)
+    document.querySelector('#nameUser').innerHTML = `${data.name}`
+    document.querySelector('#nickName').innerHTML = `@${data.login}`
+    document.querySelector('#createUser').innerHTML = `Joined ${new Date(data.created_at).toDateString()}`
+    document.querySelector('#avatarUser').src = data.avatar_url
+
+    const bio = data.bio;
+    const bioUserElements = document.querySelectorAll('#bioUser');
+    bioUserElements.forEach((element) => {
+      element.innerHTML = bio !== null && bio !== '' ? bio : 'This profile has no bio';
+    })
+
+    const publicRepoElements = document.querySelectorAll('#publicRepo');
+    publicRepoElements.forEach((element) => {
+      element.innerHTML = `${data.public_repos}`
+    })
+    const followersElements = document.querySelectorAll('#followers');
+    followersElements.forEach((element) => {
+      element.innerHTML = `${data.followers}`
+    })
+    const followingElements = document.querySelectorAll('#following');
+    followingElements.forEach((element) => {
+      element.innerHTML = `${data.following}`
+    })
+    const locationElements = document.querySelectorAll('#location');
+    locationElements.forEach((element) => {
+      element.innerHTML = `${data.location}`
+    })
+    const blogElements = document.querySelectorAll('#blog');
+    blogElements.forEach((element) => {
+      element.innerHTML = `${data.blog}`
+    })
+    const companyElements = document.querySelectorAll('#company');
+    companyElements.forEach((element) => {
+      element.innerHTML = `${data.company}`
+    })
+    const twitterIconElements = document.querySelectorAll('#twitterIcon');
+    const twitterUsernameElements = document.querySelectorAll('#twitterUsername');
+    const color = data.twitter_username === null || data.twitter_username.trim() === '' ? '#9ca3af' : 'currentColor';
+    twitterIconElements.forEach((element) => {
+      element.style.color = color;
+    })
+    twitterUsernameElements.forEach((element) => {
+      element.innerHTML = `${data.twitter_username === null ? 'Not Avaliable' : data.twitter_username}`;
+      element.style.color = color;
+    })
+  } catch (error) {
+    const errorMessageElement = document.querySelector('#error-message');
+    errorMessageElement.textContent = 'Error: User not found';
+  
+    if (errorMessageElement.classList.contains('hidden')) {
+      errorMessageElement.classList.remove('hidden');
+      
+    } else {
+      errorMessageElement.classList.add('hidden');
+    }  
+    errorMessage();
+    
+  }
+
+  
+};
+const errorMessage = () => {
+  const errorMessageElement = document.querySelector('#error-message');
+  errorMessageElement.classList.remove('hidden');
+
+  setTimeout(() => {
+    errorMessageElement.classList.add('hidden');
+  }, 5000);
+};
